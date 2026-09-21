bcbf84a6973add0845d1d4808c96859c25f3d876
ata412 <ata412@gmail.com>
2026-09-22 01:18:54 +0700

    Add cell01 exercises

---
+++ discovery_piscine/cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Tanakorn"
+last_name = "Jaempinit"
+print(first_name, last_name)
---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Tanakorn"
+last_name = "Jaempinit"
+whole_name = first_name + " " + last_name
+print(whole_name)
